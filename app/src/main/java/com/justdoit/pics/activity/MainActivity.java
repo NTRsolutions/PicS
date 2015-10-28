d70fd954a635ce4d3d94543861c67fa49f41cb73
ljzzz <otobai@vip.qq.com>
2015-10-28 23:02:29 +0800

    mainActivity使用CoordinatorLayout作为根容器,toolbar添加临时跳转UserInfoActivity按钮

--- app/src/main/java/com/justdoit/pics/activity/MainActivity.java
+++ app/src/main/java/com/justdoit/pics/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.justdoit.pics.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,12 +12,13 @@ import com.justdoit.pics.R;
 
 /**
  * 用来跳转到需要的测试页面
- * TODO:完成测试后，就要将list view的页面设置为主页面
+ * TODO:完成测试后，就要将recyclerview的页面设置为主页面
  * Created by mengwen on 2015/10/26.
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button userinfoBtn;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +29,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
-        userinfoBtn = (Button) findViewById(R.id.user_info_btn);
-
-        userinfoBtn.setOnClickListener(this);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,UserInfoActivity.class));
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.user_info_btn:
-                Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
-                startActivity(intent);
-                break;
             default:
                 break;
         }
