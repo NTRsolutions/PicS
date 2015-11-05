b24f5e85b24e4844d3de0be1b75bb67029c91395
ljzzz <otobai@vip.qq.com>
2015-11-05 16:11:54 +0800

    补全content

---
+++ app/src/main/java/com/justdoit/pics/bean/Content.java
@@ -0,0 +1,133 @@
+package com.justdoit.pics.bean;
+
+/**
+ * Created by ljz on 2015/10/30.
+ */
+public class Content {
+    private String mAvatarUrl;
+    private String mPostImageUrl;
+    private String mCommenterOneAvatarUrl;
+    private String mCommenterTwoAvatarUrl;
+    private String mCommenterThreeAvatarUrl;
+    private String mCommenterOneDisplayName;
+    private String mCommenterTwoDisplayName;
+    private String mCommenterThreeDisplayName;
+    private String mComment;
+    private String mDisplayName;
+    private String mMessage;
+    private String mTimestamp;
+    private int mCommentCount;
+    private int mPlusOneCount;
+
+    public String getmTimestamp() {
+        return mTimestamp;
+    }
+
+    public void setmTimestamp(String mTimestamp) {
+        this.mTimestamp = mTimestamp;
+    }
+
+    public String getmPostImageUrl() {
+        return mPostImageUrl;
+    }
+
+    public void setmPostImageUrl(String mPostImageUrl) {
+        this.mPostImageUrl = mPostImageUrl;
+    }
+
+    public int getmPlusOneCount() {
+        return mPlusOneCount;
+    }
+
+    public void setmPlusOneCount(int mPlusOneCount) {
+        this.mPlusOneCount = mPlusOneCount;
+    }
+
+    public String getmMessage() {
+        return mMessage;
+    }
+
+    public void setmMessage(String mMessage) {
+        this.mMessage = mMessage;
+    }
+
+    public String getmAvatarUrl() {
+        return mAvatarUrl;
+    }
+
+    public void setmAvatarUrl(String mAvatarUrl) {
+        this.mAvatarUrl = mAvatarUrl;
+    }
+
+    public String getmComment() {
+        return mComment;
+    }
+
+    public void setmComment(String mComment) {
+        this.mComment = mComment;
+    }
+
+    public int getmCommentCount() {
+        return mCommentCount;
+    }
+
+    public void setmCommentCount(int mCommentCount) {
+        this.mCommentCount = mCommentCount;
+    }
+
+    public String getmCommenterOneAvatarUrl() {
+        return mCommenterOneAvatarUrl;
+    }
+
+    public void setmCommenterOneAvatarUrl(String mCommenterOneAvatarUrl) {
+        this.mCommenterOneAvatarUrl = mCommenterOneAvatarUrl;
+    }
+
+    public String getmCommenterOneDisplayName() {
+        return mCommenterOneDisplayName;
+    }
+
+    public void setmCommenterOneDisplayName(String mCommenterOneDisplayName) {
+        this.mCommenterOneDisplayName = mCommenterOneDisplayName;
+    }
+
+    public String getmCommenterThreeAvatarUrl() {
+        return mCommenterThreeAvatarUrl;
+    }
+
+    public void setmCommenterThreeAvatarUrl(String mCommenterThreeAvatarUrl) {
+        this.mCommenterThreeAvatarUrl = mCommenterThreeAvatarUrl;
+    }
+
+    public String getmCommenterThreeDisplayName() {
+        return mCommenterThreeDisplayName;
+    }
+
+    public void setmCommenterThreeDisplayName(String mCommenterThreeDisplayName) {
+        this.mCommenterThreeDisplayName = mCommenterThreeDisplayName;
+    }
+
+    public String getmCommenterTwoAvatarUrl() {
+        return mCommenterTwoAvatarUrl;
+    }
+
+    public void setmCommenterTwoAvatarUrl(String mCommenterTwoAvatarUrl) {
+        this.mCommenterTwoAvatarUrl = mCommenterTwoAvatarUrl;
+    }
+
+    public String getmCommenterTwoDisplayName() {
+        return mCommenterTwoDisplayName;
+    }
+
+    public void setmCommenterTwoDisplayName(String mCommenterTwoDisplayName) {
+        this.mCommenterTwoDisplayName = mCommenterTwoDisplayName;
+    }
+
+    public String getmDisplayName() {
+        return mDisplayName;
+    }
+
+    public void setmDisplayName(String mDisplayName) {
+        this.mDisplayName = mDisplayName;
+    }
+}
