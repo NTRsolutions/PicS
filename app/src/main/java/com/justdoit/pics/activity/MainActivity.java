112f2f27faa664064dafbaec3eed5a883291a47c
ljz19940101 <otobai@vip.qq.com>
2015-11-09 16:27:17 +0800

    使用SupportFragmentManger

--- app/src/main/java/com/justdoit/pics/activity/MainActivity.java
+++ app/src/main/java/com/justdoit/pics/activity/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        getFragmentManager()
+        getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.container, MainFragment.newInstance("test"),"MainFragment")
                 .commit();
